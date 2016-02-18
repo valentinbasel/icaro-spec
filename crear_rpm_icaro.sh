3a79d66c6895353722fbd1381bfe4686ac818dab
vbasel <vbasel@localhost.localdomain>
2016-02-18 17:05:41 -0300

    agrego un script de carga para facilitar la creacion del SPEC

---
+++ crear_rpm_icaro.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+# clono icaro
+git clone https://github.com/valentinbasel/icaro.git
+
+
+
+#copio el spec al ~/rpmbuild/SPEC
+
+cp icaro.spec ~/rpmbuild/SPECS/
+
+# entro en la carpeta de git
+
+# tomo el SHA del ultimo commit 
+cd icaro/
+git checkout devel
+git=$(git rev-parse HEAD)
+
+# borro el rpm y voy al SOURCE
+rm -rf ~/rpmbuild/RPMS/noarch/
+cd ~/rpmbuild/SOURCES
+
+# borro todo para dejar el SOURCE limpio
+rm *.tar.gz
+
+# descargo el git con el ultimo commit
+github="https://github.com/valentinbasel/icaro/archive/"
+icaro=$github$git".zip"
+file="icaro-"$git".tar.gz"
+wget $icaro -O $pwd $file
+
+# armo el RPM
+commit="%global commit "$git 
+cd ~/rpmbuild/SPECS
+
+# borro la linea commit y la remplazo con el SHA nuevo
+sed -i "s/\%global commit .*/$commit/" icaro.spec
+
+rpmbuild -ba icaro.spec
+
+
